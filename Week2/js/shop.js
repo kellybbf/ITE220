2f7c0139e9f5b1b51836c0f93f87e0552573cb31
isah bello doro <isahbellodoro@isahs-MacBook-Air.local>
2016-03-29 16:15:49 +0700

    shop js file

---
+++ Week2/js/shop.js
@@ -0,0 +1,10 @@
+var custormerName = "joe" ;
+var price = 10 ;
+var quantity = 20;
+var totalPrice = price *quantity;
+
+var custormerElement = document.getElementById("custormer-name");
+    custormerElement.textContent = custormerName;
+
+var custormerElement = document.getElementById("total-price");
+    custormerElement.textContent = totalPrice ;
